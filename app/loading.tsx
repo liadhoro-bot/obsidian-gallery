fd02c54042bb0a7628d2be4198405b52221e40c7
Liad Horowitz <liadhoro@gmail.com>
2026-09-16 13:55:56 +0300

    Remove stale root loading shell

--- app/loading.tsx
+++
@@ -1,5 +0,0 @@
-import { V3SilverLoadingShell } from './components/v3-silver-loading-shell'
-
-export default function AppLoading() {
-  return <V3SilverLoadingShell title="Obsidian" tabs={['Workspace', 'Gallery']} cardCount={3} />
-}
